9d35274d995e8fd19df130fa1817ebb142351105
vignesh <vigneshwaran274mail@gmail.com>
2022-09-10 14:42:57 +0530

    dom

---
+++ getElementByClassName/class.js
@@ -0,0 +1,2 @@
+const x = document.getElementsByClassName("disp");
+x[1].innerHTML = "iam_w_i_k_i...!";
---
+++ getElementbytagName/tag.js
@@ -0,0 +1,6 @@
+//var x = document.getElementsByTagName("li");
+//document.getElementById("tag").innerHTML = x[0].innerHTML;
+
+
+
+document.getElementsByTagName("p")[2].innerHTML = "iam_w_i_k_i"
